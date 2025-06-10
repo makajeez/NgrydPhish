615f5187b0e6c9cdef306cd5274c6e29444cc784
Makajeez <buhari.alhassan0@gmail.com>
2025-06-10 12:53:11 +0100

    Update pdf viewer

--- src/app/gf-embed/gf-embed.component.ts
+++ src/app/gf-embed/gf-embed.component.ts
@@ -1,14 +1,28 @@
 import { CommonModule } from '@angular/common';
-import { Component } from '@angular/core';
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-import { RouterModule } from '@angular/router';
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+import {  NgxExtendedPdfViewerModule,NgxExtendedPdfViewerService, pdfDefaultOptions } from 'ngx-extended-pdf-viewer';
+import { inject } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-gf-embed',
   templateUrl: './gf-embed.component.html',
   standalone: true,
-  imports: [RouterModule, CommonModule, FormsModule, ReactiveFormsModule],
+  imports: [NgxExtendedPdfViewerModule, CommonModule],
+  providers: [NgxExtendedPdfViewerService],
+  changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class GfEmbedComponent {
+  loading: boolean = false;
+  email = '';
+  constructor(private pdfService: NgxExtendedPdfViewerService, private route: ActivatedRoute) {
 
+    pdfDefaultOptions.assetsFolder = 'bleeding-edge';
+    this.route.data.subscribe(data => {
+      this.email = data['Email'];
+      // You can now use the email value as needed
+      console.log('Email from route data:', this.email);
+    });
+    
+    }
 }
